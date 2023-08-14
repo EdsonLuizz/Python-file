dff207fdb26814d35b52b2601bc8319ab84b6d95
Edson Luiz <100296179+EdsonLuizz@users.noreply.github.com>
2023-08-14 14:03:01 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,4 @@
+m = float(input('Digite a quantidade em metros: '))
+c = m * 100
+mm = m * 1000
+print('valor em metros: {}\nvalor em centimetros: {:.2f}\nvalor em milimetros: {:.2f}'.format(m, c, mm))
