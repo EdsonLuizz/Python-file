1f3c8d7be082547c25a000b3115a9cfeff36eb1b
Edson Luiz <100296179+EdsonLuizz@users.noreply.github.com>
2023-08-14 14:02:23 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um numero: '))
+dobro = n * 2
+triplo = n * 3
+raiz = n ** (1/2)
+print('o numero escolhido foi: {}\n dobro: {}\n triplo: {}\n raiz quadrada: {:.0f}'. format(n, dobro, triplo, raiz))
