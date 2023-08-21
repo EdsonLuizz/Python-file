35cf7105f5ec58b0586bcfdf4c9db0bbd874e70a
Edson Luiz <100296179+EdsonLuizz@users.noreply.github.com>
2023-08-21 16:25:22 -0300

    Add files via upload

---
+++ ex034.py
@@ -0,0 +1,9 @@
+n1 = int(input('N1: '))
+n2 = int(input('N2: '))
+
+if n1 > n2:
+    print('o primeiro numero e maior!')
+elif n2 > n1:
+    print('o segundo numero e maior!')
+else:
+    print('os dois sao iguais!')
