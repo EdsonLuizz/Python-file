ccd15d9b8b67e0a00d7415b09139b0421e810566
Edson Luiz <100296179+EdsonLuizz@users.noreply.github.com>
2023-09-02 13:39:32 -0300

    Delete ex073.py

--- ex073.py
+++
@@ -1,6 +0,0 @@
-palavras = ('estudar', 'prova', 'minuscula')
-for c in palavras:
-    print(f'\nNa palavra {c.upper()} temos: ', end='')
-    for letra in c:
-        if letra in 'aeiou':
-            print(letra, end=' ')
