447488bce1a5143bfa6ed1ace59ff67655251c6a
Edson Luiz <100296179+EdsonLuizz@users.noreply.github.com>
2023-09-07 17:00:25 -0300

    Add files via upload

---
+++ ex079.py
@@ -0,0 +1,15 @@
+expr = str(input('Digite a expressao: '))
+pilha = []
+for s in expr:
+    if s == '(':
+        pilha.append('(')
+    elif s == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressao esta valida!!')
+else:
+    print('Sua expressao NAO esta invalida')
